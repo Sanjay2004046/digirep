4a33aa3937c30a46d0060c9908106c16461d0d0d
Sanjay2004046 <111832913+Sanjay2004046@users.noreply.github.com>
2023-05-19 16:07:27 +0530

    Add files via upload

---
+++ main/signup.js
@@ -0,0 +1,20 @@
+fname=document.getElementById("fname").value;
+lname=document.getElementById("lname").value;
+email=document.getElementById("email").value;
+password=document.getElementById("pwd").value;
+phone=document.getElementById("phone").value;
+if(fname==""){
+    alert("Fill all fields");
+}
+if(lname==""){
+    alert("Fill all fields");
+}
+if(email==""){
+    alert("Fill all fields");
+}
+if(password==""){
+    alert("Fill all fields");
+}
+if(phone==""){
+    alert("Fill all fields");
+}
